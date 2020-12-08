935d549411c10a59a3b2fd0475fd87a61af75509
Stanislav Plakhin <plakhin@users.noreply.github.com>
2020-12-08 11:17:50 +0800

    add `GET` method for logout route

--- src/routes.php
+++ src/routes.php
@@ -6,4 +6,4 @@ use Ndg0\LaravelSocialiteAuth\Http\Controllers\Auth\LoginController;
 Route::get('login/lsa/{provider}', [LoginController::class, 'redirectToProvider']);
 Route::get('login/lsa/{provider}/callback', [LoginController::class, 'handleProviderCallback']);
 Route::get('login', [LoginController::class, 'login'])->name('login');
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::match(['get', 'post'], 'logout', [LoginController::class, 'logout'])->name('logout');
